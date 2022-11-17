1ca8ddd22f0f58516d66ce8b0a9acca3bd9e079e
Mai Van Manh <mvmanh@gmail.com>
2022-11-17 16:25:27 +0700

    add student controller

---
+++ MvcWebApp/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebApp.Models;
+
+namespace MvcWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private static List<Student> products = new List<Student>();
+
+        public StudentController()
+        {
+            if (products.Count == 0)
+            {
+                products.Add(new Student() { Id = "111", Name = "Duy", Age = 18, Email = "student@tdtu.edu.vn" });
+                products.Add(new Student() { Id = "222", Name = "Loan", Age = 18, Email = "student@tdtu.edu.vn" });
+                products.Add(new Student() { Id = "333", Name = "Anh", Age = 18, Email = "student@tdtu.edu.vn" });
+                products.Add(new Student() { Id = "444", Name = "Minh", Age = 18, Email = "student@tdtu.edu.vn" });
+
+            }
+        }
+
+        
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+
+       
+
+        // GET: ProductManagement/Edit/5
+        public ActionResult Edit(string id)
+        {
+            Student p = products.FirstOrDefault(p => p.Id == id);
+            if (p == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else return View(p);
+        }
+
+        // POST: ProductManagement/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, Student s)
+        {
+            Student p = products.FirstOrDefault(p => p.Id == id);
+            if (p != null)
+            {
+                p.Name = s.Name;
+                p.Age = s.Age;
+                p.Email = s.Email;
+            }
+            
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcWebApp/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace MvcWebApp.Models
+{
+    public class Student
+    {
+        public string Id { get; set; } 
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public Student() { }
+
+        public Student(string id, string name, int age, string email)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Email = email;
+        }
+    }
+}
